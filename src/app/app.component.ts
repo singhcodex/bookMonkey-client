ca4af59f20ee4384591842813e6ea7e788d6fbf9
singhcodex <coder69developer@gmail.com>
2023-04-13 11:37:44 +0200

    input property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookmonkey-client';
+
+  book = {
+    title : 'How to write an Essay',
+    author: 'Singh',
+    abstract: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    Obcaecati at quos dolor neque corrupti aspernatur adipisci laudantium fuga numquam molestiae`
+  }
 }
---
+++ src/app/book-card/book-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.scss']
+})
+export class BookCardComponent {
+
+  @Input() content: any;
+  
+  customStyle = {
+    color: 'green'
+  };
+  
+}
