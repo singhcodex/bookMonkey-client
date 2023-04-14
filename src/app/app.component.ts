834361f67173005f4f035c3d05e053698dc448c0
singhcodex <coder69developer@gmail.com>
2023-04-14 08:57:29 +0200

    eventemitter handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,19 @@ import { Book } from './book';
 export class AppComponent {
   title = 'bookmonkey-client';
 
-  public book : Book = {
-    title : 'How to write an Essay',
-    author: 'Singh Simran',
-    abstract: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-    Obcaecati at quos dolor neque corrupti aspernatur adipisci laudantium fuga numquam molestiae`
-  }
-  
+  public books : Book[] = [
+    {
+      title : 'How to write an Essay',
+      author: 'Singh Simran',
+      abstract: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Obcaecati at quos dolor neque corrupti aspernatur adipisci laudantium fuga numquam molestiae'
+    },
+    {
+      title : 'How to make Coffee',
+      author: 'Chota Beem',
+      abstract: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Obcaecati at quos dolor neque corrupti aspernatur adipisci laudantium fuga numquam molestiae'
+    }
+  ]
+
   handleEmitFromChild(evt:Book){
     console.log(evt);
   }
